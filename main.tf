23598ff88433500368bc3f1fa9beabda545b33f7
Rojina <shrestha1987@gmail.com>
2021-01-17 17:48:19 -0500

    variable code

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 2.70"
+    }
+  }
+}
+provider "aws" {
+  profile = "default"
+  region = "us-west-1"
+}
+resource "aws_instance" "example" {
+  ami = "ami-03130878b60947df3"
+  instance_type = "t2.micro"
+  availability_zone = var.az
+}
+variable "az" {
+  type = "string"
+}

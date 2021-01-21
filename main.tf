3fcbe859155242028644dee5afad0813c00eef74
Rojina <shrestha1987@gmail.com>
2021-01-21 16:50:36 -0500

    dec2020

--- main.tf
+++ main.tf
@@ -13,12 +13,16 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "ami-0be2609ba883822ec"
   instance_type = var.instance_type
+  key_name = "dec2020"
+  subnet_id = "subnet-4dfda000"
+  security_groups = ["sg-0bd415784c1253324"]
+
 }
 
 variable "instance_type" {
-  type = string
+  type = string 
 }
+
 
 
   
-}
